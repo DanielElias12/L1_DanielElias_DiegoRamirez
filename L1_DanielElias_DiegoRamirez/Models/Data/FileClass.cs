9281e07c5479e9b249c9fbd98d2766b92ea10e10
Dandres2001 <dandres21nw@gmail.com>
2021-02-15 20:28:59 -0600

    Lectura de archivos

---
+++ L1_DanielElias_DiegoRamirez/Models/Data/FileClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L1_DanielElias_DiegoRamirez.Models.Data
+{
+    public class FileClass
+    {
+        public IFormFile csv { get; set; }
+
+    }
+}
